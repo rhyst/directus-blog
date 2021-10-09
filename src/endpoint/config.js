b911cacd7ded022e8ae7301ce5f25579af9cffec
Rhys Tyers <rhystyers1@gmail.com>
2021-10-09 13:05:23 +0100

    Refactor to full configurable routes

--- src/endpoint/config.js
+++ src/endpoint/config.js
@@ -1,56 +1,84 @@
-const nunjucks = (nunjucks, env, config) => {
+const nunjucks = (nunjucks, env) => {
   env.addGlobal("customFunction", () => "hello world");
 };
 
-module.exports = {
-  extensionName: "blog", // Should match folder name /extensions/endpoints/<extensionName> and /extensions/hooks/<extensionName>
-  title: "My Site", // Website title for HTML title tag
-  baseUrl: "/blog", // Base URL of the blog
-  // DB info
-  collection: "post", // Directus collection to source blog items from
-  index: {
-    view: "index.njk", // Nunjucks template for rendering index
-    fields: ["date", "name", "summary", "content", "slug", "tags"], // Item fields to select for passing to template
-    filter: (req) => ({ published: { _eq: true }, indexed: { _eq: true } }), // Filter items that are passed to template
-  },
-  rss: {
-    view: "rss.njk", // Nunjucks template for rendering rss
-    fields: ["date", "name", "summary", "content", "slug"], // Item fields to select for passing to template
-    filter: { published: { _eq: true }, indexed: { _eq: true } }, // Filter items that are passed to template
-  },
-  item: {
-    view: "item.njk", // Nunjucks template for rendering single item
-    fields: ["date", "name", "content", "slug"], // Item fields to select for passing to template
-    slugField: "slug", // Field that the item slug is stored in
-    // Filter items that are passed to template
-    // <slug> can be null
-    filter: (slug) => ({
-      ...(slug ? { slug: { _eq: slug } } : {}),
-      published: { _eq: true },
-    }),
-    // Callback before item is rendered
-    beforeRender: (item, { ItemsService }) => {
-      // Example of pulling content from another collection
-      if (item?.link?.length) {
-        const contentFields = { collectionName: "someField" };
-        const link = item.link[0];
-        const linkedService = new ItemsService("post", { schema });
-        const contentField = contentFields[link.collection];
-        const linked = await linkedService.readOne(link.item, {
-          fields: [contentField],
+module.exports = async (router, extensionContext, utils) => {
+  const schema = await extensionContext.getSchema();
+
+  const slugify = async (context) => {
+    const keys = Array.isArray(context.item) ? context.item : [context.item];
+    for (let key of keys) {
+      const service = new extensionContext.services.ItemsService(
+        context.collection,
+        {
+          schema,
+        }
+      );
+      const item = await service.readOne(key);
+      if (item.name && !item.slug) {
+        const slug = utils.slugify(`${item.date}-${item.name}`, {
+          lower: true,
         });
-        item.content = linked[contentField];
+        await service.updateOne(key, { slug });
+        console.log(`Updated slug of ${context.collection} ${key} to ${slug}`);
       }
+    }
+  };
+
+  return {
+    extensionName: "blog",
+    baseUrl: "/blog",
+    collection: "post",
+    routes: [
+      {
+        view: "rss.njk",
+        url: "/index.xml",
+        filters: { published: { _eq: true }, indexed: { _eq: true } },
+        beforeResponse: (_, __, res) =>
+          res.setHeader("content-type", "application/atom+xml"),
+      },
+      {
+        view: "index.njk",
+        url: "/:page(\\d{0,})",
+        limit: 10,
+        filters: { published: { _eq: true }, indexed: { _eq: true } },
+      },
+      {
+        view: "index.njk",
+        url: ["/tags/:tags_contains", "/tags/:tags_contains/:page(\\d{0,})"],
+        limit: 10,
+        filters: { published: { _eq: true }, indexed: { _eq: true } },
+      },
+      {
+        view: "item.njk",
+        url: "/posts/:slug",
+        filters: { published: { _eq: true } },
+      },
+      {
+        view: "index.njk",
+        url: ["drafts", "/drafts/:page(\\d{0,})"],
+        limit: 10,
+        filters: { published: { _eq: false }, indexed: { _eq: true } },
+        auth: true,
+      },
+      {
+        view: "item.njk",
+        url: "/drafts/posts/:slug",
+        filters: { published: { _eq: false } },
+        auth: true,
+      },
+    ],
+    notFound: {
+      view: "404.njk",
     },
-  },
-  404: {
-    view: "404.njk",
-  },
-  // For RSS
-  authorName: "",
-  authorEmail: "",
-  // Output
-  minify: true,
-  // Add custom nunjucks stuff
-  nunjucks,
+    hooks: {
+      "items.create": slugify,
+      "items.update": slugify,
+    },
+    // Any other custom values
+    // The entire config object is passed to all templates
+    title: "My Site", // Website title for HTML title tag
+    authorName: "", // For default RSS template
+    authorEmail: "", // For default RSS template
+  };
 };
